fbc74ddf708fca64b25e1c5dc9eb9b1ff8a36320
Daniil Panov <from4life@pm.me>
2022-04-07 19:45:10 +0700

    Add new rounds to playRound func

--- game.js
+++ game.js
@@ -5,13 +5,23 @@ function computerPlay () {
     return rsp[a]
 }
 
-console.log(computerPlay());
-
 function playRound(playerSelection, computerSelection) {
-    let playerSelection = playerSelection.toLowerCase();
-    let computerSelection = computerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        return 'You lose! Paper beats Rock'
+        return 'You lose! Paper beats Rock';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors beats Paper';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock beats Scissors';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose! Scissors beats Paper';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper beats Rock';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose! Rock beats Scissors';
+    } else if (playerSelection === computerSelection) {
+        return 'We have a tie!';
     }
 }
