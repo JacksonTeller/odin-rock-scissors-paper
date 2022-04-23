d739e0cb6f74e68903081b952b22acd69271c98a
Daniil Panov <from4life@pm.me>
2022-04-23 10:58:31 +0700

    Display running score

--- game.js
+++ game.js
@@ -29,36 +29,46 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+let playerPoints = 0, computerPoints = 0;
+
+let humanScore = document.querySelector('.humanScore');
+let pcScore = document.querySelector('.pcScore');
+humanScore.innerText = humanScore.innerText + " " + playerPoints;
+pcScore.innerText = pcScore.innerText + " " + computerPoints;
+
 // the main game function
 function game(playerSelection) {
-    //let playerPoints
+
+    //display round result
     let rndResult = document.querySelector(".rndResult");
     rndResult.innerText = "Round result:";
     
-
-    //let playerSelection = prompt('Rock, Scissors or Paper?');
+    //get choices from player and computer
     let computerSelection = computerPlay();
     let result = playRound(playerSelection, computerSelection);
     rndResult.innerText = rndResult.innerText + " " + result;
 
     if (result.slice(0,7) === 'You win') {
-        playerPoints++
-    console.log(`You: ${playerPoints}\nComputer: ${computerPoints}`)
+        playerPoints++;
+        humanScore.innerText = humanScore.innerText.replace(/\d/, playerPoints);
+        console.log(`You: ${playerPoints}\nComputer: ${computerPoints}`)
     } else if (result.slice(0,7) === 'You los') {
         computerPoints++
+        pcScore.innerText = pcScore.innerText.replace(/\d/, computerPoints);
         console.log(`You: ${playerPoints}\nComputer: ${computerPoints}`)
     }
 }
-/*
+
 let buttons = document.querySelectorAll("button");
 
 buttons.forEach(button => button.addEventListener('click', () => {
     game(button);
 }));
-*/
 
-let playerPoints = document.querySelector('.humanScore');
 
+//let playerPoints = document.querySelector('.humanScore');
+
+//console.log(playerPoints.innerText);
 
 
 
@@ -75,6 +85,4 @@ let buttonR = document.querySelector('.r');
 buttonR.addEventListener('click', playRound);
 */
 
-
-console.log(playerPoints.innerText);
 
